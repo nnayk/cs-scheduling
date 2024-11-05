6dd88fb0862223520aff4af4eda96d83bc34a4a8
nnayk <79224511+nnayk@users.noreply.github.com>
2024-11-05 12:14:42 -0800

    Initially create questions page

---
+++ frontend/src/pages/questions.js
@@ -0,0 +1,13 @@
+import Agreement_Questions from "./agreement_questions";
+import Written_Questions from "./written_questions";
+
+const Questions = () => {
+  return (
+    <div>
+      <Agreement_Questions />
+      <Written_Questions />
+    </div>
+  );
+};
+
+export default Questions;
